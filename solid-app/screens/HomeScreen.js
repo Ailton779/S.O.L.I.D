19425437eceff096dd1d61a964a9ad0caf6b30a4
Ailton <130189317+Ailton779@users.noreply.github.com>
2026-09-08 06:00:04 +0000

    fix: corrige caminho da logo na HomeScreen

--- solid-app/screens/HomeScreen.js
+++ solid-app/screens/HomeScreen.js
@@ -49,7 +49,7 @@ export default function HomeScreen({ navigation }) {
       <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
 
       <Animated.View style={[styles.logoContainer, { opacity: logoOpacity, transform: [{ scale: logoScale }] }]}>
-        <Image source={require('./assets/logo.png')} style={styles.logo} resizeMode="contain" />
+        <Image source={require('../assets/logo.png')} style={styles.logo} resizeMode="contain" />
       </Animated.View>
 
       <Animated.Text style={[styles.title, { opacity: titleOpacity }]}>S.O.L.I.D</Animated.Text>
